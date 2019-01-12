ae765ba401894ac47d20c4bfe2d16368f58c075d
Shaun Raj <shaunraj@gmail.com>
2019-01-12 16:46:21 -0500

    Added mtgtop8 library

--- EventFinder.js
+++ EventFinder.js
@@ -18,22 +18,26 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
+        var cmd = args[0];       
         args = args.splice(1);
         switch(cmd) {
             // !ping
-            case 'ping':
+            case 'modern':
+                //Get Data For Modern Tournaments
+                
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
-            break;
-            // Just add any case commands if you want to..
+                break;
+            case 'standard':
+                //Get Data for Standard Tournaments
+                break;
+            case 'legacy':
+                //Get Data for Legacy Tournaments
+                break;          
          }
      }
 });
